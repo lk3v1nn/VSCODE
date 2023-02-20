a5482cb636e2a6ccccafa1704e0293f0368240d1
lk3v1nn <kevinlmorataya@gmail.com>
2023-02-19 20:25:57 -0600

    gps

--- interfaz 2.py
+++ interfaz 2.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://api.chat-api.com/666231808616863/sendMessage?token=EAAJd7y5KaZA8BAFTEZBoXhUbpaZBxnKnf1lmrgkauZBZAugpf4EgZBbKxkQuih6Hf6W74TbEPfKeWZAH1kvHLZCvZAYBkdmOmyfjyKGtjZBIl1NaSiERYorCSoWoViIA9AoTP4etW523ZCyXAvQNkM0fBAVRODCnQx9qasGRasdJYikImU027NL1165Hr1HFatz6kOu9ZCzcwdhengZDZD'
+url = 'https://api.chat-api.com/101042539575372/sendMessage?token=EAAJd7y5KaZA8BAME1o6zoF8JSlQVrgBU2kyxSia7rrXAtCYRM1CZCvZCH2yw5R3ZCfHZBZAY7qf1fxCQ675ALLGHYL7tsNGjFygykPghv4nE6pQMNGBglexrbWkDaddtFvZCWtGTaXNvJvIgd5fVRfPVbBdCvclMVSz9XW8chwT7JkSQX5IsjRr4HE4ZCLfYfAymCbyaSD5evAZDZD'
 datos = {
     "phone": "50256327243",
     "body": "¡Hola! ¡Este es un mensaje de prueba enviado desde Python!"
